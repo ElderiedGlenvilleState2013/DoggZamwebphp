b89a3554e5fabc2ed29457f068f65d63ef255510
Elderied McKinney <mckinneydemetrice@icloud.com>
2019-03-10 00:43:52 -0500

    finishing final
    doggzam added new buttons

---
+++ main.php
@@ -0,0 +1,55 @@
+<<!DOCTYPE html>
+<html>
+<head>
+
+
+    
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>DoggZam! </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css">
+    <script src="main.js"></script>
+
+
+    <form action="PHP8808.php" method="Post">
+
+    First Name: <input type="text" name="first_name"/>
+        Last Name: <input type="text" name="last_name" />
+
+        <br>
+
+        <input type="submit"  />
+
+    </form>
+</head>
+
+
+<h1> DoggZam! </h1>
+
+<body>
+
+<h1>Sign In</h1>
+    
+    <section class="signInForm">
+
+    <form method="Request" action="index.html">
+        email: <input name="email" type="text" /> 
+    password: <input name="password" type="password" /> <br> <br>
+
+         <br>       <input type="submit" />
+
+
+    </form>
+    </section>
+
+
+<br>
+<br>
+<br>
+<img src="image/DoggZamIcon.jpg"> <br>
+
+echo ("welcome to DoggZam1");
+    
+</body>
+</html>
---
+++ openingpage.php
@@ -0,0 +1,63 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+    <link href="app.css" rel="stylesheet">
+
+    
+
+    
+        
+</head>
+
+
+
+<img src="image/doggz2Logo.png"> <br>
+<br>
+<br>
+<br>
+
+
+<body>
+
+
+    
+    
+    <section class="signInForm">
+        <h1>Sign In</h1>
+            <form method="Request" action="index.html">
+                email: <input name="email" type="text" /> 
+                password: <input name="password" type="password" /> <br> <br>
+
+         <br>       <input type="submit" />
+
+
+    </form>
+    </section>
+   
+<br>
+<br>
+<br>
+<img src="image/DoggZamIcon.jpg"> <br>
+
+
+
+<script type language="javascript">
+        document.write("welcome to DoggZam "  +". <break>");
+            var username=prompt("Please enter username: ", " ");
+            var passWord=prompt("Enter Password :", " ");
+            if (passWord== ""){
+                document.write("you must wrtie a valid password");
+            } else if (passWord==" password"){
+                document.write("welcome back");
+
+                
+            }
+
+    </script>
+</body>
+
+<footer>FasTek Technologies LLC/DoggZam!</footer>
+</html>
